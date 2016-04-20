04bb0d277dde4be9c98df0ac7815de48f610307d
Ryan Cox <ryan@Ryans-MBP.hsd1.ca.comcast.net>
2016-04-19 22:28:33 -0700

    clean up

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 $(function(undefined) {
-  var count = 50000,
+  var count = 20; //50000;
 
-      // The second parameter is the seed, remove it to get a different
-      // set and order on page refresh. That is:
-      // var nodes = generateNodes(50000);
-      nodes = generateNodes(50000, 1234567),
+  // The second parameter is the seed, remove it to get a different
+  // set and order on page refresh. That is:
+  // var nodes = generateNodes(50000);
+  nodes = generateNodes(count, 1234567);
 
-      content = '';
-      totalEdges = _.reduce(nodes, function(count, value) {
-          return count + _.size(value);
-      }, 0),
-      averageEdgeCount = (totalEdges / count).toFixed(2);
+  content = '';
 
-  content = [
+  totalEdges = _.reduce(nodes, function(count, value) {
+      return count + _.size(value);
+  }, 0),
+  averageEdgeCount = (totalEdges / count).toFixed(2);
+
+  var content = [
     'There are a total of',
     totalEdges.toLocaleString(), 'total edges',
     'for', count.toLocaleString(), 'nodes.',
