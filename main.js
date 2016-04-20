2ea15252f1b1d32d5bd8511a1bfd3e1070ed7f6a
Ryan Cox <ryan@Ryans-MBP.hsd1.ca.comcast.net>
2016-04-19 23:03:29 -0700

    with UL

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ $(function(undefined) {
   // The second parameter is the seed, remove it to get a different
   // set and order on page refresh. That is:
   // var nodes = generateNodes(50000);
-  nodes = generateNodes(count, 1234567);
-
-  content = '';
+  var nodes = generateNodes(count, 1234567);
 
   totalEdges = _.reduce(nodes, function(count, value) {
       return count + _.size(value);
@@ -21,4 +19,8 @@ $(function(undefined) {
   ].join(' ');
 
   $('.container').html(content);
+
+  for (var node in nodes) {
+    $('.node-list').append('<li>' + node + '</li>');
+  }
 });
